ea3dbab2a3fd05951f5385ee7eb017ad153a5748
kimerein <kimerein@gmail.com>
2017-12-15 18:47:46 -0500

    fixing

--- setup_reach_coding_settings.m
+++ setup_reach_coding_settings.m
@@ -15,8 +15,8 @@ settings.save=1; % 1 if want to save zones data to movie directory, 0 otherwise
 % Note that the location of these zones should not change during the movie
 settings.zones(1).name='perch zone'; % zone name
 settings.zones(1).prompt='Draw a polygon enclosing the perch zone on the image.'; % describe zone to user
-settings.zones(1).analysisField='perchZone'; % refers to field for automated movie analysis from readIntensityValues.m on
-% see ____ for explanation of how different fields are used
+settings.zones(1).analysisField='perchZone'; % refers to field for automated movie analysis from readIntensityValues.m onward
+% see autoReachAnalysisSettings.m for explanation of how different analysis fields are used
 settings.zones(1).takeImageValue='intensity'; % which value to get from this zone in movie
 % takeImageValue can be one of the following:
 % 'intensity' : get intensity from movie pixels
@@ -26,24 +26,30 @@ settings.zones(1).takeImageValue='intensity'; % which value to get from this zon
 
 settings.zones(2).name='LED zone';
 settings.zones(2).prompt='Draw a polygon enclosing the distractor LED zone on the image.';
+settings.zones(2).analysisField='LEDZone';
 settings.zones(2).takeImageValue='intensity';
 
 settings.zones(3).name='reach zone';
 settings.zones(3).prompt='Draw a polygon enclosing the area surrounding but NOT including the pellet.';
+settings.zones(3).analysisField='reachZone';
 settings.zones(3).takeImageValue='intensity';
 
 settings.zones(4).name='eat zone';
 settings.zones(4).prompt='Draw a polygon enclosing the region where mouse paws go when mouse eats.';
+settings.zones(4).analysisField='eatZone';
 settings.zones(4).takeImageValue='intensity';
 
 settings.zones(5).name='pellet zone';
 settings.zones(5).prompt='Draw a polygon enclosing the stopped pellet.';
+settings.zones(5).analysisField='pelletZone';
 settings.zones(5).takeImageValue='intensity';
 
 settings.zones(6).name='cue zone';
 settings.zones(6).prompt='Draw a polygon enclosing the area illuminated when the cue turns on.';
+settings.zones(6).analysisField='cueZone';
 settings.zones(6).takeImageValue='blue';
 
 settings.zones(7).name='opto zone';
 settings.zones(7).prompt='Draw a polygon enclosing the area illuminated when the optogenetic stimulus turns on.';
+settings.zones(7).analysisField='optoZone';
 settings.zones(7).takeImageValue='red';
