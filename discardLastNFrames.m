2f6e0a51c6a9cd6cc3e79af381e90236f5d7ad12
kimerein <kimerein@gmail.com>
2018-01-12 10:55:48 -0500

    fixed bug in small segment alignment

--- discardLastNFrames.m
+++ discardLastNFrames.m
@@ -6,22 +6,25 @@ discardLastN=settings.discardLastN;
 if discardLastN==0
     return
 end
-savehandles.discardLastN=discardLastN;
 totalFrames=sum(~isnan(savehandles.LEDvals))+savehandles.discardFirstNFrames;
 lastNFrames=totalFrames-discardLastN+1:totalFrames;
 
+framesInLED=sum(~isnan(savehandles.LEDvals));
+
 f=fieldnames(savehandles);
 for i=1:length(f)
-    if sum(~isnan(savehandles.(f{i})))==sum(~isnan(savehandles.LEDvals))
+    if sum(~isnan(savehandles.(f{i})))==framesInLED
         % Each ind is a frame
         lastNotNan=find(~isnan(savehandles.(f{i})),1,'last');
         temp=savehandles.(f{i});
         temp(lastNotNan-discardLastN+1:lastNotNan)=nan;
         savehandles.(f{i})=temp;
-    else
+    elseif ~ischar(savehandles.(f{i}))
         % Event refer to inds
         temp=savehandles.(f{i});
         temp=temp(~(temp>=lastNFrames(1) & temp<=lastNFrames(end)));
         savehandles.(f{i})=temp;
     end
 end
+
+savehandles.discardLastN=discardLastN;
