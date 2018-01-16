d821522b5ca370c17f5d72248ab6a2ada04f1432
kimerein <kimerein@gmail.com>
2018-01-16 18:59:51 -0500

    fixed bug in movieframeinds alignment

--- plotCueTriggeredBehavior.m
+++ plotCueTriggeredBehavior.m
@@ -161,7 +161,8 @@ for i=plot_cues
             temp=tbt.(nameOfCue);
             event_ind_cue=find(temp(i,:)>event_thresh,1,'first');
             event_ind_pellet=find(tbt.pelletPresented(i,:)>event_thresh);
-            if event_ind_pellet(end)>length(timespertrial) || event_ind_cue>length(timespertrial)
+            if isempty(event_ind_pellet)
+            elseif event_ind_pellet(end)>length(timespertrial) || event_ind_cue>length(timespertrial)
             elseif any((timespertrial(event_ind_pellet)-timespertrial(event_ind_cue))>0 & (timespertrial(event_ind_pellet)-timespertrial(event_ind_cue))<settings.blockITIThresh)
                 % Fast block
                 lastTrialShaded=2;
